ffb00177779d796a40731ff0275e5d9425e619fd
Joan <Joanpadolina@gmail.com>
2018-12-02 20:46:41 +0100

    Initial commit

---
+++ js/accountvalid.js
@@ -0,0 +1,10 @@
+console.log("hello")
+function buttonAni(){
+  var button = document.getElementsByClassName('submitlog');
+  var form = document.getElementsByName('form');
+
+  if(form==valid){
+    button.classList.add('buttonshow');
+  }
+
+}
