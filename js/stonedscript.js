100ca7c66afffdc74e4139363a867fffe1f488fd
Joan <Joanpadolina@gmail.com>
2019-01-06 20:16:28 +0100

    testing commit
    de commit werkt niet
    +smoke bij verhalen

--- js/stonedscript.js
+++ js/stonedscript.js
@@ -1,12 +1,11 @@
-
 animusjs.init();
 
-
 function active(element, options) {
-	if(options.status === 'animate') {
-		element.classList.add('active');
-	}
-	if(options.status === 'reverse') {
-		element.classList.remove('active');
-	}
+  if(options.status === 'animate') {
+    element.classList.add('active');
+  }
+  if(options.status === 'reverse') {
+    element.classList.remove('active');
+  }
 }
+console.log("oo");
