5e2742f8a8a12a45ec4fc47c4566e5e63a0784d6
Joan <Joanpadolina@gmail.com>
2019-01-09 14:07:28 +0100

    Verhalenpagina eerste versie

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,35 @@
 var ster = document.querySelector('.bookmark');
 
-ster.onclick=function(){
-    ster.classList.toggle('bookmark-color');
+// ster.onclick = function() {
+//   ster.classList.toggle('bookmark-color');
+//
+// };
 
-};
+function toggleClass( element ) {
+    ster.classList.toggle('bookmark-color');
+}
+
+ster.addEventListener('click', toggleClass);
+
+
 
 var hidezoek = document.querySelector('.zoekbalkboven');
 var search = document.querySelector('.searchl');
 
-search.onclick=function(){
-    search.classList.toggle('search-move');
-    hidezoek.classList.toggle('show-zoekbalk');
-    }
+search.onclick = function() {
+  search.classList.toggle('search-move');
+  hidezoek.classList.toggle('show-zoekbalk');
+};
+
+var filterAnim = document.querySelector('.filter');
+var filterKnop = document.querySelector('.filterknop');
+var toonFilt = document.querySelector('#zoek');
+
+
+filterKnop.onclick = function() {
+  filterAnim.classList.toggle('filter-show');
+};
+
+toonFilt.onclick = function(){
+  filterAnim.classList.toggle('filter-unshow');
+};
