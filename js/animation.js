7667036926a06c2edd56627b5baf53bb09764ce6
Joan <Joanpadolina@gmail.com>
2019-01-20 18:39:23 +0100

    + Chat active gif 2e microinteractie + icoontjes gelijk getrokken

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,13 @@
-
-
 // Ster met popuanimatie
 
 var ster = document.querySelector('.bookmark');
-var popup= document.querySelector('.steranimatie');
+var popup = document.querySelector('.steranimatie');
 var spanophaal = document.querySelector('span');
 
 function toggleClass() {
-    ster.classList.toggle('bookmark-color');
-    popup.classList.toggle('sterappear');
-    spanophaal.classList.toggle('spananimate');
+  ster.classList.toggle('bookmark-color');
+  popup.classList.toggle('sterappear');
+  spanophaal.classList.toggle('spananimate');
 }
 
 ster.addEventListener('click', toggleClass);
@@ -20,10 +18,12 @@ ster.addEventListener('click', toggleClass);
 var hidezoek = document.querySelector('.zoekbalkboven');
 var search = document.querySelector('.searchl');
 
-search.onclick = function() {
-  search.classList.toggle('search-move');
+
+function searchreveal() {
   hidezoek.classList.toggle('show-zoekbalk');
 }
+search.addEventListener('click', searchreveal);
+
 
 // Filter verschijnt mobile version
 
@@ -39,3 +39,27 @@ filterKnop.onclick = function() {
 toonFilt.onclick = function() {
   filterAnim.classList.toggle('filter-unshow');
 }
+//comment verschijnt wanneer er gedrukt
+
+//
+//chat gif aankijkt
+var commentgif = document.querySelector('.chatgif');
+var chatknop = document.querySelector('.chat');
+var chatcontainer = document.querySelector('.containchatfave');
+var reviewtext = document.querySelector('.commentverschijn');
+function chatgifappear() {
+    commentgif.classList.toggle('show-comment-gif');
+    chatcontainer.classList.toggle('show-reviewcontent', 'show-comment');
+    chatknop.style.display = 'none';
+    reviewtext.style.display = 'block';
+
+}
+function chatdelete(){
+    commentgif.classList.remove('show-comment-gif');
+    chatcontainer.classList.remove('show-reviewcontent');
+    chatknop.style.display ='block';
+    reviewtext.style.display = 'none';
+}
+
+chatknop.addEventListener('click', chatgifappear);
+commentgif.addEventListener('click', chatdelete);
